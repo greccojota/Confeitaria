16d4c5349ff5c3c3742f540677f1d9311f4e00d6
Joao Victor Grecco <jvgrecco@hotmail.com>
2022-02-09 21:57:17 -0300

    instalando e configurando BD postgres

--- Receita/settings.py
+++ Receita/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Receita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'receita',
+        'USER': 'postgres',
+        'PASSWORD': 'j@Postgres22'
+        'HOTS': 'localhost'
     }
 }
 
--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
  # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    receitas = {
+        1:'Bolo de Chocolate',
+        2:'Bolo de Laranja',
+        3:'Bolo de Iogurte',
+        4:'Bolo de Milho'
+    }
+
+    dados = {
+        'nome_receitas': receitas
+    }
+
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
